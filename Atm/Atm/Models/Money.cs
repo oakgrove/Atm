e927a05fb195e20a1e646cde85cb3305904d7051
Sebastian Mebius <sebastian.mebius@gmail.com>
2015-09-28 17:06:44 +0200

    Money added

---
+++ Atm/Atm/Models/Money.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Atm.Models
+{
+    public class Money
+    {
+        public int Id { get; set; }
+        public int Denominator { get; set; }
+    }
+}
