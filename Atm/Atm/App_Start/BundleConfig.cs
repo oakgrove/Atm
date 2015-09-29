1a7be80f029b479f95c7f58f80e3b9c031e381e2
Sebastian Mebius <sebastian.mebius@gmail.com>
2015-09-29 08:58:10 +0200

    Added Foundation 5 etc.

--- Atm/Atm/App_Start/BundleConfig.cs
+++ Atm/Atm/App_Start/BundleConfig.cs
@@ -3,6 +3,7 @@ using System.Web.Optimization;
 
 namespace Atm
 {
+    // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
@@ -14,18 +15,17 @@ namespace Atm
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+                        "~/Scripts/foundation.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/foundation.css",
+                      "~/Content/app.css"));
         }
     }
 }
