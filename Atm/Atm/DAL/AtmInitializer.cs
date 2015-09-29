de7e10c4399870d477c56fd5204d457fdf929006
Johan Järvenhag <j.jarvenhag@gmail.com>
2015-09-29 11:09:24 +0200

    Added transaction seed in AtmInitializerz

--- Atm/Atm/DAL/AtmInitializer.cs
+++ Atm/Atm/DAL/AtmInitializer.cs
@@ -45,22 +45,65 @@ namespace Atm.DAL
             money.ForEach(m => context.Money.Add(m));
             context.SaveChanges();
 
-            //var courses = new List<Course>
-            //{
-            //    new Course {CourseID = 1050, Title = "Chemistry",      Credits = 3, },
-            //    new Course {CourseID = 4022, Title = "Microeconomics", Credits = 3, },
-            //    new Course {CourseID = 4041, Title = "Macroeconomics", Credits = 3, },
-            //    new Course {CourseID = 1045, Title = "Calculus",       Credits = 4, },
-            //    new Course {CourseID = 3141, Title = "Trigonometry",   Credits = 4, },
-            //    new Course {CourseID = 2021, Title = "Composition",    Credits = 3, },
-            //    new Course {CourseID = 2042, Title = "Literature",     Credits = 4, }
-            //};
-            //courses.ForEach(s => context.Courses.AddOrUpdate(p => p.Title, s));
-            //context.SaveChanges();
+            var transactions = new List<Transaction>();
 
+            foreach (var account in accounts)
+            {
+                DateTime date = DateTime.Now;
+                double bal = account.Balance;
+                string type = "";
 
+                for (int i = 0; i < 30; i++)
+                {
+                    double amount = rnd.Next(1, 20 + 1) * 100;
 
+                    if (rnd.Next(0, 2) == 0)
+                    {
+                        type = "Uttag";
+                    }
+                    else
+                    {
+                        type = "Insättning";
+                    }
+
+                    date = date.AddDays(rnd.Next(-5, 1));
+
+                    transactions.Add(new Transaction { TransactionTime = date, TransactionType = type, Balance = bal, Amount = amount, Account = account });
+
+                    if (type == "Uttag") bal = bal + amount;
+                    else bal = bal - amount;
+                }
+            }
+            transactions.ForEach(t => context.Transactions.Add(t));
+            context.SaveChanges();
         }
-    }
 
+
+        //            [Display(Name = "Tid")]
+        //public DateTime TransactionTime { get; set; }
+        //[Display(Name = "Typ av transaktion")]
+        //public string TransactionType { get; set; }
+        //[Display(Name = "Saldo")]
+        //public double Balance { get; set; }
+        //[Display(Name = "Belopp")]
+        //public double Amount { get; set; }
+
+        //public BankAccount Account { get; set; }
+        //var courses = new List<Course>
+        //{
+        //    new Course {CourseID = 1050, Title = "Chemistry",      Credits = 3, },
+        //    new Course {CourseID = 4022, Title = "Microeconomics", Credits = 3, },
+        //    new Course {CourseID = 4041, Title = "Macroeconomics", Credits = 3, },
+        //    new Course {CourseID = 1045, Title = "Calculus",       Credits = 4, },
+        //    new Course {CourseID = 3141, Title = "Trigonometry",   Credits = 4, },
+        //    new Course {CourseID = 2021, Title = "Composition",    Credits = 3, },
+        //    new Course {CourseID = 2042, Title = "Literature",     Credits = 4, }
+        //};
+        //courses.ForEach(s => context.Courses.AddOrUpdate(p => p.Title, s));
+        //context.SaveChanges();
+
+
+
+    }
 }
+
